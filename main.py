1c0531d036859a676b4cef800a8abf77e235a72d
zooksman <zooksman@gmail.com>
2016-09-15 18:35:49 -0500

    final no styles

--- main.py
+++ main.py
@@ -44,21 +44,56 @@ class MainHandler(webapp2.RequestHandler):
 
 class Index(MainHandler):
 	def get(self):
-		
-		self.render("front.html")
+		self.render("create.html", title_error = self.request.get("title_error"), art_error = self.request.get("art_error"))
 
 class ListPosts(MainHandler):
     def get(self):
     	five_posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
-    	five_posts = [five_posts]
-        self.render("list.html", five_posts = five_posts)
+    	# five_posts = [five_posts]
+        self.render("list.html", posts = five_posts, header = "Recent Posts")
         
 class AddPost(MainHandler):
 	def post(self):
-		
-		self.render("create.html")
+		post_title = self.request.get("title")
+		post_content = self.request.get("art")
+		is_title_error = False
+		is_art_error = False
+		title_error = ""
+		art_error = ""
+		error = ""
+		if (not post_title) or (post_title.strip() == ""):
+			title_error = "Please enter a title."
+			is_title_error = True
+		if (not post_content) or (post_content.strip() == ""):
+			art_error = "Please enter some art."
+			is_art_error = True
+		if is_title_error:
+			if error != "":
+				error += "&"
+			error += "title_error=" + title_error
+		if is_art_error:
+			if error != "":
+				error += "&"
+			error += "art_error=" + art_error
+		if is_art_error or is_title_error:
+			self.redirect("/?" + error)
+		else:
+			newpost = Post(title = post_title, content = post_content)
+			newpost.put()
+			self.redirect("/blog/" + str(newpost.key().id()))
+			
+class ViewPostHandler(MainHandler):
+    def get(self, id):
+    	id = int(id)
+    	if not Post.get_by_id(id):
+    		self.response.write("404 post not found")
+        else: 
+        	self.render("list.html", posts = [Post.get_by_id(id)], header = "Post #" + str(id))
+
 app = webapp2.WSGIApplication([
 	('/', Index),
     ('/blog', ListPosts),
-    ('/newpost', AddPost)
+    ('/newpost', AddPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+
 ], debug=True)
